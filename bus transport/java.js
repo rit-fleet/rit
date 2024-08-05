a20dff79c2b9c7a570254d0b831b6ca3298e1d5a
rit-fleet <rit.transport.fleet@gmail.com>
2024-08-05 10:45:42 +0530

    Add files via upload

---
+++ bus transport/java.js
@@ -0,0 +1,4 @@
+// Add event listener to menu button to toggle popup menu
+document.getElementById('menu-button').addEventListener('click', function() {
+    document.getElementById('popup-menu').classList.toggle('show');
+  });
