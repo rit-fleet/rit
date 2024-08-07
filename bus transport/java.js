7a986b2f53fba23995f67b471b4263669eeb19fb
rit-fleet <rit.transport.fleet@gmail.com>
2024-08-07 11:29:00 +0530

    Delete bus transport/java.js

--- bus transport/java.js
+++
@@ -1,4 +0,0 @@
-// Add event listener to menu button to toggle popup menu
-document.getElementById('menu-button').addEventListener('click', function() {
-    document.getElementById('popup-menu').classList.toggle('show');
-  });
